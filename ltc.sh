0698fed81a426295d1022398e01ce9019f5ae06e
Yogandovi01 <31580579+Yogandovi01@users.noreply.github.com>
2017-09-03 10:20:14 +0700

    Create ltc.sh

---
+++ ltc.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+
+echo "Updating..."
+sudo apt-get update
+echo " "
+echo "Installing libcurl4"
+sudo apt-get install libcurl4-openssl-dev git
+echo " "
+echo "Installing build-essential"
+sudo apt-get install build-essential
+echo " "
+echo "Make Dir Downloads"
+mkdir downloads
+cd downloads
+echo "Clone CpuMiner"
+git clone https://github.com/pooler/cpuminer
+echo " "
+echo "Configure CpuMiner"
+cd cpuminer
+./autogen.sh
+CFLAGS="-march=native" ./configure
+make
+make install
+
+echo " "
+echo "** Configuring CpuMiner done."
+echo "** for start mining run command : minerd -o stratum+tcp://ltc.pool.minergate.com:3336 -u YOUREMAIL -p x"
+echo " "
+echo "Script by Gh0st32 - Alosia Team"
